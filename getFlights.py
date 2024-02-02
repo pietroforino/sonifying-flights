19c378507fb0964de969407cf3b48b7e79d21241
ariutti <nicola.ariutti@dotdotdot.it>
2024-02-02 12:45:18 +0100

    jsonification of data

--- getFlights.py
+++ getFlights.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python3
+
 from FlightRadar24 import FlightRadar24API
+import json
 
 fr_api = FlightRadar24API()
 
@@ -10,4 +13,46 @@ bounds = fr_api.get_bounds_by_point(lat, lon, radius)
 
 flights = fr_api.get_flights(bounds = bounds)
 
-print("Flying to", flights)
+
+flightsDictList = []
+
+for f in flights:
+    flightString = str(f)
+    # get rid of annoying characters
+    flightString = flightString.replace('<', '').replace('>','')
+
+    # split by delimitator
+    fieldList = [ a.strip() for a in flightString.split('-')]
+
+    #print( len( fieldList ) )
+    if len( fieldList ) == 5:
+        # agggreget first an sendon element
+        aggregate = fieldList[0] + '-' + fieldList[1]
+        #print( fieldList )
+        #print( aggregate )
+        # get rid of second element
+        fieldList.pop(1)
+        # and replace first element with aggragate
+        fieldList[0] = aggregate
+    else:
+        # leave as it is
+        pass
+
+
+    dictionary = {
+        "flight_id": fieldList[0],
+        fieldList[1].split(':')[0].strip(): fieldList[1].split(':')[1].strip(),
+        fieldList[2].split(':')[0].strip(): fieldList[2].split(':')[1].strip(),
+        fieldList[3].split(':')[0].strip(): fieldList[3].split(':')[1].strip()
+    }
+
+    #print( dictionary )
+    flightsDictList.append( dictionary )
+
+
+OUPUT_FILE = "myjson.json"
+
+print( flightsDictList )
+
+with open(OUPUT_FILE, "w") as outfile:
+    json.dump(flightsDictList, outfile)
