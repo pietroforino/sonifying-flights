0f6b401232cc20b3bf7f7b284ede5d1bbcfa89cd
ariutti <nicola.ariutti@dotdotdot.it>
2024-02-02 16:54:58 +0100

    prime mappature

--- main.js
+++ main.js
@@ -98,177 +98,6 @@ async function startLoop() {
   */
 }
 
-/*
-function playNote(
-  {
-    time,
-    note,
-    velocity = 0.15,
-    duration = 1.15,
-    attack = 0.02,
-    decay = 0.1,
-    sustain = 0.5,
-    release = 5.0,
-    type = "triangle",
-    delayTime = 0.2,
-    delayFeedback = 0.7,
-  } = {},
-  destination = audioCtx.destination
-) {
-  // Create
-  let osc = audioCtx.createOscillator();
-  let noiseGain = audioCtx.createGain();
-  let gain = audioCtx.createGain();
-  let delay = audioCtx.createDelay();
-  let delayFeedbackGain = audioCtx.createGain();
-
-  // Configure
-  osc.type = type;
-  osc.frequency.value = mtof(note);
-  noiseGain.gain.value = 0.2;
-  gain.gain.value = 0;
-  gain.gain.setValueAtTime(0, time);
-  gain.gain.linearRampToValueAtTime(velocity, time + attack);
-  gain.gain.linearRampToValueAtTime(velocity * sustain, time + attack + decay);
-  gain.gain.setValueAtTime(velocity * sustain, time + duration);
-  gain.gain.setTargetAtTime(0, time + duration, release * 0.2);
-  delay.delayTime.value = delayTime;
-  delayFeedbackGain.gain.value = delayFeedback;
-
-  // Connect
-  osc.connect(gain);
-  gain.connect(destination);
-
-  // Start
-  osc.start(time);
-  osc.stop(time + duration + release);
-}
-
-function playOneShotBuffer({ time, buffer, gain = 0.5, rate = 1.0 }) {
-  // Create
-  let bufferSrcNode = audioCtx.createBufferSource();
-  let gainNode = audioCtx.createGain();
-
-  // Configure
-  bufferSrcNode.buffer = buffer;
-  bufferSrcNode.playbackRate.value = rate;
-  gainNode.gain.value = gain;
-
-  // Connect
-  bufferSrcNode.connect(gainNode);
-  gainNode.connect(audioCtx.destination);
-
-  // Start
-  bufferSrcNode.start(time);
-}
-
-function randRange(min = 0, max = 1) {
-  return min + Math.random() * (max - min);
-}
-
-function startMelody(length, octaveShift, destination) {
-  let chordNoteIndexes = [0, 2, 4, 6];
-  let noteIndex = 0;
-  let sequence = [];
-  for (let i = 0; i < length; i += 1) {
-    if (Math.random() < 0.2) {
-      sequence.push(-1);
-    } else {
-      if (noteIndex === 0) noteIndex += 1;
-      else if (noteIndex === chordNoteIndexes.length - 1) noteIndex -= 1;
-      else noteIndex += Math.random() < 0.5 ? -1 : 1;
-      let note = SCALE[chordNoteIndexes[noteIndex]] + 12 * octaveShift;
-      sequence.push(note);
-    }
-  }
-
-  let seqIndex = 0;
-  let event = clock
-    .callbackAtTime((event) => {
-      let note = sequence[seqIndex++ % sequence.length];
-      if (note === -1) return;
-      let energyOctaveShift = -1 + Math.floor(energy * 3);
-      let timingHumanisation = randRange(0.0, 0.02);
-      let delayTimes = [0.4, 0.3, 0.2];
-      let delayTime = delayTimes[Math.floor(Math.random() * delayTimes.length)];
-      playNote(
-        {
-          time: event.deadline + timingHumanisation,
-          note: note + 12 * energyOctaveShift,
-          velocity: randRange(0.03, 0.06),
-          duration: randRange(0.05, 0.15),
-          attack: randRange(0.01, 0.02),
-          decay: 0.0,
-          sustain: 1.0,
-          release: randRange(0.25, 1.0),
-          type: "triangle",
-          delayFeedback: 0.5 + energy * 0.4,
-          delayTime,
-        },
-        destination
-      );
-      visualiseEvent({ time: event.deadline, isSmall: true });
-    }, audioCtx.currentTime)
-    .repeat(0.1);
-  clockEvents.push(event);
-}
-
-function visualiseEvent({ time, hue }) {
-  let delay = Math.max(0, time - audioCtx.currentTime);
-  setTimeout(() => {
-    let x = randRange(0, window.innerWidth - 200);
-    let y = randRange(0, window.innerHeight - 200);
-    let saturation = 80 + energy * 20;
-    let lightness = 50 + energy * 30;
-    let color = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-    let eventEl = document.createElement("div");
-    eventEl.classList.add("event");
-    eventEl.style.left = `${x}px`;
-    eventEl.style.top = `${y}px`;
-    eventEl.style.backgroundColor = color;
-    visContainer.appendChild(eventEl);
-    eventEl
-      .animate([{ opacity: 0.5 }, { opacity: 0 }], {
-        duration: 3000,
-        easing: "ease-out",
-        fill: "forwards",
-      })
-      .finished.then(() => eventEl.remove());
-  }, delay);
-}
-
-function startNoteLoop(note, initialDelay, interval, visHue, noteParams = {}) {
-  let event = clock
-    .callbackAtTime((event) => {
-      let energyOctaveShift = -1 + Math.floor(energy * 3);
-      playNote({
-        time: event.deadline,
-        note: note + 12 * energyOctaveShift,
-        delayFeedback: 0,
-        ...noteParams,
-      });
-      visualiseEvent({ time: event.deadline, hue: visHue });
-    }, audioCtx.currentTime + initialDelay)
-    .repeat(interval);
-  clockEvents.push(event);
-}
-
-async function startBufferLoop(url, gain, rate, initialDelay, interval) {
-  let buffer = await loadBuffer(url);
-  let event = clock
-    .callbackAtTime(async (event) => {
-      playOneShotBuffer({ time: event.deadline, buffer, gain, rate });
-      visualiseEvent({ time: event.deadline });
-    }, audioCtx.currentTime + initialDelay)
-    .repeat(interval);
-  clockEvents.push(event);
-}
-
-function updateEnergy(value) {
-  energy = value;
-}
-*/
-
 async function startEverything() {
   await audioCtx.resume();
 
@@ -277,122 +106,114 @@ async function startEverything() {
   //saturator.connect(audioCtx.destination);
   clock.start();
 
-/*
+
   // load loops
   for (let i=0; i < loopUrl.length; i++) {
     console.log( loopUrl[i] );
     loopBuffers.push( await loadBuffer( loopUrl[i] ) );
   }
-  console.log( loopBuffers );
+  //console.log( loopBuffers );
 
+/*
   // load oneshots
   for (let i=0; i < oneshotUrl.length; i++) {
     console.log( oneshotUrl[i] );
     oneshotBuffers.push( await loadBuffer( oneshotUrl[i] ) );
   }
-  console.log( oneshotBuffers );
-
-  // now create the buffers source nodes
-  for (let i=0; i < loopBuffers.length; i++) {
-    let bufferSrcNode = audioCtx.createBufferSource();
-    bufferSrcNode.buffer = loopBuffers[i];
-    bufferSourceNodes_loopsList.push( bufferSrcNode )
-  }
-  console.log( bufferSourceNodes_loopsList )
-  */
-
-  await startLoop();
-
-  /*
-  loopBuffers.push( await loadBuffer("/loops/loop_1.mp3") );
-  loopBuffers.push( await loadBuffer("/loops/loop_2.mp3") );
-  loopBuffers.push( await loadBuffer("/loops/loop_3.mp3") );
-  loopBuffers.push( await loadBuffer("/loops/loop_4.mp3") );
-  loopBuffers.push( await loadBuffer("/loops/loop_5.mp3") );
-  loopBuffers.push( await loadBuffer("/loops/loop_6.mp3") );
-
-  let weatherData = await fetch("/weatherdataset.json").then((r) => r.json());
-  let tempo = 0.25;
-
-  // Yearly gong
-  startBufferLoop(
-    "/oneshots/OS_UTP2_C_Harmony_Pluck.mp4",
-    0.5,
-    1,
-    0,
-    365 * tempo
-  );
-
-  // Seasonal gong
-  let seasonStarts = [90, 180, 270, 360];
-  for (let s of seasonStarts) {
-    startBufferLoop(
-      "/oneshots/OS_MNL_C_Percussive_Pluck.mp4",
-      0.5,
-      1,
-      s * tempo,
-      365 * tempo
-    );
-  }
-
-  // Play rainy days
-  for (let i = 0; i < weatherData.precipitation.length; i++) {
-    if (weatherData.precipitation[i].value > 0) {
-      startNoteLoop(SCALE[0], i * tempo, 365 * tempo, 200);
-    }
-  }
-
-  // Play windy days
-  for (let i = 0; i < weatherData.windspeed.length; i++) {
-    if (weatherData.windspeed[i].value > 4) {
-      startNoteLoop(SCALE[2], i * tempo, 365 * tempo, 100, {
-        type: "square",
-      });
-    }
-  }
-
-  // Vary energy based on temperature
-  let allTemperatures = weatherData.temperature.map((t) => t.value);
-  let minTemperature = Math.min(...allTemperatures);
-  let maxTemperature = Math.max(...allTemperatures);
-  let day = 0;
-  clock
-    .callbackAtTime(() => {
-      let temperature = weatherData.temperature[day % 365].value;
-      let relativeTemperature =
-        (temperature - minTemperature) / (maxTemperature - minTemperature);
-      updateEnergy(relativeTemperature);
-      day++;
-    }, audioCtx.currentTime)
-    .repeat(tempo)
-    .tolerance({ early: 0 });
+  //console.log( oneshotBuffers );
   */
 }
 
 
-function destoyPrevSounds() {
-  console.log("destroy sounds");
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
 }
 
 
-function emitNewSounds( data ) {
-  console.log( data.length );
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1);
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c; // Distance in km
+  return d;
+}
 
-  for( let i=0; i < data.length; i++) {
+
+
+
+function lerp( min, max, value) {
+  return min + value * (max - min);
+}
+
+let currentFlightData = {};
+
+async function emitNewSounds( data ) {
+  console.log("emit new sounds")
+
+
+  for( let i=0; i < currentFlightData.length; i++) {
+    if( currentFlightData[i]["bufferNode"] ) {
+      currentFlightData[i]["bufferNode"].stop();
+    }
+  }
+  currentFlightData = {};
+
+
+  currentFlightData = data;
+  let volume = 1.0 / currentFlightData.length;
+
+
+  //console.log( currentFlightData.length );
+
+  for( let i=0; i < currentFlightData.length; i++) {
     // for every flight lets create a new sound
-    let groundSpeed = data[i]["Ground Speed"];
-    let id = data[i]["flight_id"];
-    let heading = data[i]["Heading"];
-    let lat = data[i]["Latitude"];
-    let long = data[i]["Longitude"];
+    let id = currentFlightData[i]["flight_id"];
+
+    let volumeByGS = Math.min(1.0, currentFlightData[i]["Ground Speed"] / 700);
+
+    let volumeByAltitude =  getDistanceFromLatLonInKm(currentFlightData[i]["Latitude"], currentFlightData[i]["Longitude"], clickedCoordinates[1], clickedCoordinates[0]);
+
+    //console.log( currentFlightData[i]["Latitude"], currentFlightData[i]["Longitude"], clickedCoordinates[1], clickedCoordinates[0] );
+    //console.log( volumeByAltitude );
+
+    currentFlightData[i]["bufferNode"] = audioCtx.createBufferSource();
+    currentFlightData[i]["gainNode"] = audioCtx.createGain();
+    currentFlightData[i]["filterNode"] = audioCtx.createBiquadFilter();
+    currentFlightData[i]["stereoPanner"] = audioCtx.createStereoPanner();
 
 
+    // Configure
+    currentFlightData[i]["bufferNode"].buffer = loopBuffers[ Math.floor( Math.random()*loopBuffers.length)];
+    currentFlightData[i]["bufferNode"].loop = true;
+    currentFlightData[i]["bufferNode"].playbackRate.value = 1.0;
+    currentFlightData[i]["gainNode"].gain.value = 1.0; // volume
+
+    currentFlightData[i]["filterNode"].type = "bandpass";
+    currentFlightData[i]["filterNode"].frequency.value = lerp(1000, 10000, currentFlightData[i]["Altitude"]/45000);
+    currentFlightData[i]["filterNode"].Q.value = 100;
+
+    currentFlightData[i]["stereoPanner"].pan.value = Math.cos( currentFlightData[i]["Heading"] * Math.PI / 180.0 );
 
 
-    console.log( id );
+    // Connect
+    currentFlightData[i]["bufferNode"].connect( currentFlightData[i]["filterNode"] );
+    currentFlightData[i]["filterNode"].connect( currentFlightData[i]["gainNode"] );
+    currentFlightData[i]["gainNode"].connect( currentFlightData[i]["stereoPanner"] );
+    currentFlightData[i]["stereoPanner"].connect(audioCtx.destination);
+
+    // start
+    currentFlightData[i]["bufferNode"].start();
   }
-}
 
+  console.log( currentFlightData );
+
+}
 
 
 
@@ -413,10 +234,10 @@ async function toggleAudio() {
     return;
   }
 
-  destoyPrevSounds();
+  //await destoyPrevSounds();
   // according to the flight we are tracking,
   // let's create corresponding sounds
-  emitNewSounds( flightData );
+  await emitNewSounds( flightData );
 }
 
 
@@ -449,7 +270,7 @@ async function sendFlightRequest(lat, long, r) {
       }).then(jsonResponse=>{
           flightData = jsonResponse
           //return( flightData )
-          console.log(flightData)
+          //console.log(flightData)
       }
       ).catch((err) => console.error(err));
 }
@@ -472,10 +293,16 @@ var map = new mapboxgl.Map({
     pitch: 30
 });
 
+
+let clickedCoordinates = [];
+
 map.on('click', function (e) {
-  var clickedCoordinates = [e.lngLat.lng, e.lngLat.lat];
-  updateDome(clickedCoordinates, 20);
+  clickedCoordinates = [];
+  clickedCoordinates = [e.lngLat.lng, e.lngLat.lat];
+  updateDome(clickedCoordinates, 50);
+  toggleAudio();
 });
+
 map.on('mousemove', (e) => {
   document.getElementById('info').innerHTML =
   // `e.point` is the x, y coordinates of the `mousemove` event
@@ -487,7 +314,7 @@ map.on('mousemove', (e) => {
   });
 
 function updateDome(center, raggio) {
-  console.log(center)
+  //console.log(center)
   sendFlightRequest(center[1], center[0], raggio * 1000)
   const options = { steps: 64, units: 'kilometers' };
   var grid = turf.hexGrid(turf.bbox(turf.circle(center, raggio, options)), precision);
